a3d3054035323a48275bbbe37d4fea5233b00420
cactus <kyle.coding@outlook.com>
2021-01-09 23:56:50 -0500

    Finished drop down menu for nav bar
    I think I made it pretty messy trying to get this finished, but I learned some things to try next time that might make it a little easier.

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,7 +1,27 @@
-const menuBtn = document.querySelector('menu-btn');
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+const home = document.querySelector('.home');
+const dropdownnav = document.querySelector('.drop-down-nav');
+const closeBtn = document.querySelector('.close-btn');
 
+let showMenu = false;
 menuBtn.addEventListener('click', toggleMenu);
+closeBtn.addEventListener('click', toggleMenu);
 
 function toggleMenu() {
+    if (!showMenu) {
+        menuBtn.classList.add('open');
+        menu.classList.add('open'); 
+        home.classList.add('open');
+        dropdownnav.classList.add('open');
+        showMenu = true;
+    } else {
+        menuBtn.classList.remove('open');
+        menu.classList.remove('open');
+        home.classList.remove('open');
+        dropdownnav.classList.remove('open');
+        showMenu = false;
+    }
     
+
 }
